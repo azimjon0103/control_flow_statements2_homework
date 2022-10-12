495483032fb28f71a58cb54f5b72554c6a190025
azimjon0103 <azimjonfayzullayev3@gmail.com>
2022-10-13 00:42:00 +0500

    05

--- if05.py
+++ if05.py
@@ -6,4 +6,26 @@ def main(n):
     Returns:
         int: return answer.
     """
-    return
+    x1=n%10
+    n//=10
+    x2=n%10
+    n//=10
+    x3=n%10
+    n//=10
+    x4=n%10
+    n//=10
+    x5=n%10
+    if x1<x2:
+        if x2<x3:
+            if x3<x4:
+                if x4<x5:
+                    return x5
+                else :
+                    return x4
+            else :
+                return x3
+        else :
+            return x2
+    else :
+        return x1                            
+print(main(23456))
