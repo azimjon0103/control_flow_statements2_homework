3c60f4d1f79f7df69cef7c27b7f1a3d8c4266726
azimjon0103 <azimjonfayzullayev3@gmail.com>
2022-10-13 09:11:33 +0500

    05

--- if05.py
+++ if05.py
@@ -16,16 +16,40 @@ def main(n):
     n//=10
     x5=n%10
     if x1<x2:
-        if x2<x3:
-            if x3<x4:
-                if x4<x5:
-                    return x5
-                else :
-                    return x4
-            else :
-                return x3
-        else :
-            return x2
+        c= x2
     else :
-        return x1                            
-print(main(23456))
+        c= x1
+    if x1<x3:
+        c= x3
+    else :
+        c= x1
+    if x1<x4:
+        c= x4
+    else :
+        c= x1    
+    if x1<x5:
+        c= x5
+    else :
+        c= x1
+    if x2<x3:
+        c= x3 
+    else :
+        c= x2
+    if x2<x4:
+        c= x4
+    else :
+        c= x2
+    if x2<x5:
+        c= x5
+    else :
+        c= x2
+    if x3<x4:
+        c= x4
+    else :
+        c= x3
+    if x4<x5:
+        c= x5 
+    else :
+        c= x4
+    return c                                                                 
+print(main(98527))
